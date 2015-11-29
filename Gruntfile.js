89d453b7dc61d52008505a3800db08e668682fda
Lucas Lauriano de Souza <lucaslauriano@live.com>
2015-11-29 06:08:05 -0200

    commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -319,7 +319,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       dist: {
         options: {
-          module: 'cntWebApp',
+          module: 'cntWebAppApp',
           htmlmin: '<%= htmlmin.dist.options %>',
           usemin: 'scripts/scripts.js'
         },
